a2224f3bbcae207f89b739962e69c87e1574f738
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2021-04-29 14:56:47 +0700

    fix asynchronus

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -4,13 +4,13 @@ const app = require("../app");
 
 describe("Get route", () => {
   it("page return welcome to node-user-auth", async (done) => {
-    const res = await request(app).get("/")
-    expect(res.statusCode).toEqual(200)
-    done()
+    const res = await request(app).get("/");
+    expect(res.statusCode).toEqual(200);
+    done();
   });
 });
 
 afterAll(async () => {
+  await app.close();
   await mongoose.connection.close();
-  app.close();
 });
